187f3203f071a78886d6e63dcd4d73c1a742783f
Martin Poulsen <mapo0004@stud.kea.dk>
2024-11-21 13:57:46 +0100

    add to git

---
+++ src/main/java/stud/kea/dk/thursdag/ThursdagApplication.java
@@ -0,0 +1,13 @@
+package stud.kea.dk.thursdag;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThursdagApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThursdagApplication.class, args);
+    }
+
+}
---
+++ src/test/java/stud/kea/dk/thursdag/ThursdagApplicationTests.java
@@ -0,0 +1,13 @@
+package stud.kea.dk.thursdag;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThursdagApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
